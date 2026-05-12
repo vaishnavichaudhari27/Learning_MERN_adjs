67589947e91dc2ef0391716fe325d770773b9b35
vaishnavichaudhari27 <chaudharivaishnavi882@gmail.com>
2026-05-12 12:18:39 +0530

    ad js

---
+++ js/libraries.js
@@ -0,0 +1,11 @@
+export let message = "Es6 Module";
+
+export function user(name){
+    console.log(`Hello my name ${name}`);
+}
+
+export class test{
+    constructor(){
+        console.log("This is test class counstructor")
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import {message, user ,test} from "./libraries.js";
+
+console.log(message);
+user("vaishnavi")
+
+let a = new test ();
+
